be329033a23f7a393af6c606a2c4c957ca7ba9f6
TE <TE@localhost.localdomain>
2019-02-11 12:12:52 +0530

    assignmentC2 start

---
+++ assignmentC2/src/main/BankersAlgorithm.java
@@ -0,0 +1,18 @@
+package main;
+
+public class BankersAlgorithm {
+
+	
+	boolean safetyAlgorithm() {
+		return true;
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		BankersAlgorithm b = new BankersAlgorithm();
+		boolean stat = b.safetyAlgorithm();
+		
+	}
+
+}
